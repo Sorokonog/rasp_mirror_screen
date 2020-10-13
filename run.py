402b477d3e08dfc9022117554298bea234d43e76
Roman <roman@voltbro.com>
2020-10-13 13:36:29 +0300

    toggler added

--- run.py
+++ run.py
@@ -6,13 +6,19 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(14, GPIO.IN)
 
+toggle = 0
+
 while True:
     i=GPIO.input(14)
-    if i==0:                 #When output from motion sensor is LOW
-        print ("mirrow",i)
-        os.system("sudo vcgencmd display_power 0")
-        time.sleep(0.1)
-    elif i==1:               #When output from motion sensor is HIGH
-        print ("ads",i)
-        os.system("sudo vcgencmd display_power 1")  #Turn MirrowScreen ON
-        time.sleep(0.1)
+    if i==0: #When output from motion sensor is LOW
+        if toggle == 0:                 
+            print ("mirrow",i)
+            os.system("sudo vcgencmd display_power 0")
+            time.sleep(0.1)
+            toggle = 1
+    elif i==1: #When output from motion sensor is HIGH
+        if toggle == 1:       
+            print ("ads",i)
+            os.system("sudo vcgencmd display_power 1")  #Turn MirrowScreen ON
+            time.sleep(0.1)
+            toggle = 0
